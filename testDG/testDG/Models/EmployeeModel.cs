b9f68f56f72650a006bf47a2635bba108f1e5faf
Noob-programming <rbygsg8@gmail.com>
2023-10-01 16:16:25 +0300

    mvp in test

---
+++ testDG/testDG/Models/EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testDG.Models
+{
+    internal class EmployeeModel
+    {
+        public Guid EmployeeGuid { get; set; }
+        public int EmployeeId { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public DateTime Date { get; set; }  
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ testDG/testDG/View/Interfaces/IEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testDG.View.Interfaces
+{
+    internal interface IEmployee
+    {
+        Guid EmployeeGuid { get; set; }
+        int EmployeeId { get; set; }
+        string EmployeeFirstName { get; set; }
+        string EmployeeLastName { get; set; }
+        DateTime Date { get; set; }
+        string PhoneNumber { get; set; }
+    }
+}
