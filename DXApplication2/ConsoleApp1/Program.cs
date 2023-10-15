738c9ea03b15471ba9f22ac467c3f5e097890a9f
Noob-programming <rbygsg8@gmail.com>
2023-10-15 17:18:40 +0300

    res

--- DXApplication2/ConsoleApp1/Program.cs
+++ DXApplication2/ConsoleApp1/Program.cs
@@ -1,9 +1,15 @@
-﻿var s = new[] { "a", "b" };
-Console.WriteLine(s.Count());
-Play();
-return;
+﻿
+Console.WriteLine(5.IsEven());
+Console.WriteLine(6.IsEven());
+const int x = 10;
+const int y = 11;
+Console.WriteLine(x.IsEqual(y));
+Console.WriteLine(x.Objester());
 
-void Play()
+internal static class Ex
 {
-	Console.WriteLine("Play");
+	public static bool IsEven(this int x) => x % 2 == 0;
+
+	public static bool IsEqual(this int x, int y) => x == y;
+	public static bool Objester(this object? x) => x == null;
 }
