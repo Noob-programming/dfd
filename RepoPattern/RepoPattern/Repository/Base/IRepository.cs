b7097974831b19a1caf82f97630b625a8d9209e6
Noob-programming <rbygsg8@gmail.com>
2023-10-24 18:49:21 +0300

    repo

---
+++ RepoPattern/RepoPattern/Repository/Base/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RepoPattern.Repository.Base
+{
+	public interface IRepository<T> where T : class
+	{
+		T GetByGuid(Guid guid);
+
+		IEnumerable<T> GetAll();
+
+	}
+}
---
+++ RepoPattern/RepoPattern/Repository/MainRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using RepoPattern.Repository.Base;
+
+namespace RepoPattern.Repository
+{
+	public class MainRepo<T> : IRepository<T> where T : class
+	{
+		/// <inheritdoc />
+		public T GetByGuid(Guid guid)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <inheritdoc />
+		public IEnumerable<T> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
